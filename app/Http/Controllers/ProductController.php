41b9af148eb32efbc3c1c8333a195c6e68dc31b7
Ramazan Yousufi <yousufi.ramazan@gmail.com>
2023-04-02 22:30:58 +0430

    the new eComm add

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+ 
+    //
+         function index(){
+        return "the password of email does not match";
+         }
+}
